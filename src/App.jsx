41659486b06790ced742e7beb16065c56eb2ff7b
C0FFEEbreak <121966788+C0FFEEbreak@users.noreply.github.com>
2026-02-12 18:03:21 -0500

    Comment changes

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,6 @@ function MyHook() {
 const myFetchVar = async () => {
   try {
     setLoading(true);
-    // This API is strictly about food and very stable
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     
     if (!response.ok) throw new Error("Server error");
